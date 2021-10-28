c47721146a1560d3a3b184c2d96c2862ddace06d
EmiBusso <emiliabusso@gmail.com>
2021-10-28 15:11:44 -0700

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://mystifying-archimedes-2ecfd3.netlify.app/">
+          {" "}
+          Emilia Busso{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/EmiBusso/react-weather-app"
+          target="_blanck"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
