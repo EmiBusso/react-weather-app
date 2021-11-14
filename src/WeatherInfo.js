c19219a1587627216dc7cd5510ac971838cfdd8c
EmiBusso <emiliabusso@gmail.com>
2021-11-13 22:23:18 -0800

    Fixed icon and temperature alignment

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,15 +13,17 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
-        <div className="col-6">
+      <div className="row mt-5 mb-5">
+        <div className="col-2">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
-            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="float-left">
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
         <div className="col-6">
